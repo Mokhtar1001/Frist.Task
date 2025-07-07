e23358bd1a559fa7f1bb528f4a7a2874341cf94a
Mokhtar Ahmed <Mokhtar Ahmed@DESKTOP-OUDDGVE>
2025-07-07 03:09:39 +0300

    Add project files.

---
+++ Frist.Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frist.Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter the number of Small carpet :  ");
+            int SmallCarpet = int.Parse(Console.ReadLine());
+
+            Console.Write("enter the number of large carpet :  ");
+            int LargeCarpet = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the price of small carpet = $25 \n the price of large carpet = $35 ");
+
+            double PSCarpet = SmallCarpet * 25;
+            double PLCarpet = LargeCarpet * 35;
+
+            double cost = PSCarpet + PLCarpet;
+            double Tax = cost * 0.06;
+            double total = cost + Tax;
+
+            Console.WriteLine($"cost : ${cost} \n + Tax : ${Tax} ");
+            Console.WriteLine($"total estimated : {total} \n This estimate is valid for 30 days");
+
+        }
+    }
+}
